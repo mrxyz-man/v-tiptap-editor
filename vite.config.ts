ad4728784a98a18eac57e1af735e6f53af2dca73
mrxyz <Kazan1n.iliya@yandex.ru>
2024-08-14 21:19:54 +0500

    css inject plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,13 @@ import { defineConfig } from 'vite';
 import vue2 from '@vitejs/plugin-vue2';
 import Components from 'unplugin-vue-components/vite';
 import { VuetifyResolver } from 'unplugin-vue-components/resolvers';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     target: 'es2015',
+    cssCodeSplit: false,
     lib: {
       entry: resolve(__dirname, 'lib/index.js'),
       name: 'VTiptapEditor',
@@ -37,6 +39,7 @@ export default defineConfig({
     Components({
       resolvers: [VuetifyResolver()],
     }),
+    cssInjectedByJsPlugin(),
   ],
   resolve: {
     alias: {
